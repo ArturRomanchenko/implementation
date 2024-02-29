4cf9a483b2c0ef25e644c010a682b1502d4ba545
artur <vags2154@gmail.com>
2024-03-01 00:38:53 +0100

    clean up

---
+++ build/CMakeFiles/wstring.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wstring.dir/string/src/main.cpp.o"
+  "CMakeFiles/wstring.dir/string/src/main.cpp.o.d"
+  "wstring"
+  "wstring.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wstring.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/wstring.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for wstring.
+# This may be replaced when dependencies are built.
